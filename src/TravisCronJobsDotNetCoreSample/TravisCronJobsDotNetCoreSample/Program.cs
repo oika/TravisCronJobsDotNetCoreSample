00883f5a0da307133722f5600bb6568eccf7157d
oika <942093+oika@users.noreply.github.com>
2019-04-24 17:21:09 +0900

    add source files.

---
+++ src/TravisCronJobsDotNetCoreSample/TravisCronJobsDotNetCoreSample/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TravisCronJobsDotNetCoreSample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"{DateTime.Now} Executed.");
+        }
+    }
+}
